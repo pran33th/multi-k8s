3ebb60b5ea270de559a6710233e01980a557ff7c
Praneeth Nelavalli <praneethnelavalli@Praneeths-MacBook-Pro.local>
2023-06-01 19:46:15 +0530

    new prod test

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <p></p>
           <a
             className="App-link"
