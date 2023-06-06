6318dcbc9aad762a492740bbc3df715b83443cdd
Praneeth Nelavalli <praneethnelavalli@Praneeths-MacBook-Pro.local>
2023-06-06 16:52:19 +0530

    added deployment fix

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ docker push pr2n33th/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=pr2n33th/multi-server:$SHA
-kubectl set image deployments/client-deployment server=pr2n33th/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=pr2n33th/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=pr2n33th/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=pr2n33th/multi-worker:$SHA
