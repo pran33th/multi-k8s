5ab2a02c0f013748725b95924fd3f377b3c88497
Praneeth Nelavalli <praneethnelavalli@Praneeths-MacBook-Pro.local>
2023-06-06 16:39:25 +0530

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t pr2n33th/multi-client:latest -t pr2n33th/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t pr2n33th/multi-server:latest -t pr2n33th/multi-server:$SHA -f ./server/Dockerfile: ./server
+docker build -t pr2n33th/multi-worker:latest -t pr2n33th/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push pr2n33th/multi-client:latest
+docker push pr2n33th/multi-server:latest
+docker push pr2n33th/multi-worker:latest
+
+docker push pr2n33th/multi-client:$SHA
+docker push pr2n33th/multi-server:$SHA
+docker push pr2n33th/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=pr2n33th/multi-server:$SHA
+kubectl set image deployments/client-deployment server=pr2n33th/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=pr2n33th/multi-worker:$SHA
